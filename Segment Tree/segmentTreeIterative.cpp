f49d2324822404746dcdcfd10a08bf19b72d7425
Andrew Flik <41635993+andrewflik@users.noreply.github.com>
2019-11-11 20:49:04 +0530

    Update segmentTreeIterative.cpp

--- Segment Tree/segmentTreeIterative.cpp
+++ Segment Tree/segmentTreeIterative.cpp
@@ -15,20 +15,16 @@ t fp(t n, t p){
 	return res;
 }
 void build(int *t, int s, int h){
-	for(int i=h-1; i>=1; i--)	
-		t[i] = t[i<<1] + t[i<<1|1];	
+	for(int i=h-1; i>=1; i--) t[i] = t[i<<1] + t[i<<1|1];	
 }
 void update(int f, int *t, int index, int v){
-	for(t[index+f]=v, f+=index; f>1; f>>=1)	
-		t[f>>1] = t[f] + t[f^1];
+	for(t[index+f]=v, f+=index; f>1; f>>=1)	t[f>>1] = t[f] + t[f^1];
 }
 int query(int f, int *t, int ql ,int qr){
 	int res = 0;
 	for(ql+=f, qr+=f; ql <=qr; ql>>=1, qr>>=1){
-		if(ql&1)
-			res += t[ql++];
-		if(!(qr&1))
-			res += t[qr--];
+		if(ql&1) res += t[ql++];
+		if(!(qr&1)) res += t[qr--];
 	}
 	return res;
 }
